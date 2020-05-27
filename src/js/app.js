c07baaccc8dda35de08abbd25d29869ea2cdb6e6
danwarman <me@danwarman.co.uk>
2020-05-27 11:56:55 +0100

    ADD: Draft of JS structure

---
+++ src/js/app.js
@@ -0,0 +1,17 @@
+/**
+ * This will be the main app controller
+ */
+
+keys.addEventListener('click', (e) => {
+  const key = e.target;
+  console.log('Key:', key);
+
+  // 
+  // 1. Determine 'display result' of key input
+  // 2. Determine 'display equation' of key input
+  // 3. Update state of calculator to allow for subsequent events
+  // 4. Update UI for 'display result'
+  // 5. Update UI for 'display equation'
+  // 6. Update UI for 'keypad state'
+
+});
