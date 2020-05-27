42218ea1314766a14c34acc2a1c3fb57f84f028a
danwarman <me@danwarman.co.uk>
2020-05-27 14:47:51 +0100

    ADD: Get/Update Display & placeholder for ui controller fn

--- src/js/app.js
+++ src/js/app.js
@@ -9,6 +9,17 @@ import {
   updateDisplayEquation
 } from './helpers/ui';
 
+import {
+  handleDisplay
+} from './controllers/ui'
+
+const state = {
+  firstValue: '',
+  modifiedSecondValue: '',
+  previousKeyType: '',
+  operatorAction: ''
+};
+
 keys.addEventListener('click', (e) => {
   const key = e.target;
   console.log('Key:', key);
@@ -16,17 +27,24 @@ keys.addEventListener('click', (e) => {
   const uiResult = getDisplayResult();
   const uiEquation = getDisplayEquation();
 
-  // 
   // 1. Determine 'display result' of key input
   // 2. Determine 'display equation' of key input
+  const newResultData = handleDisplay(
+    key,
+    uiResult,
+    uiEquation,
+    state
+  );
+  const newResult = newResultData.result;
+  const newEquation = newResultData.equation;
 
   // 3. Update state of calculator to allow for subsequent events
 
   // 4. Update UI for 'display result'
-  updateDisplayResult('');
+  updateDisplayResult(newResult);
 
   // 5. Update UI for 'display equation'
-  updateDisplayEquation('');
+  updateDisplayEquation(newEquation);
 
   // 6. Update UI for 'keypad state'
 
