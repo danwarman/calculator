6e7b99d6e12738018c4cfb63c66b026c7e9dc50d
danwarman <me@danwarman.co.uk>
2020-05-27 14:55:16 +0100

    CHANGE: Update getKeyType fn

--- src/js/helpers/keyData.js
+++ src/js/helpers/keyData.js
@@ -1,6 +1,12 @@
 export const getKeyType = (key) => {
-  // TODO
-  return key;
+  const { dataset } = key;
+
+  if (!dataset) return 'number';
+  if (dataset.decimal) return 'decimal';
+  if (dataset.action && dataset.action === 'clear') return 'clear';
+  if (dataset.action && dataset.action === 'save') return 'save';
+  if (dataset.operator && dataset.operator !== 'equals') return 'operator';
+  if (dataset.operator && dataset.operator === 'equals') return 'equals';
 };
 
 export const getKeyContent = (key) => {
