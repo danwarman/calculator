b21da078efd3f867d3d01d3f47baeaa696e9fb47
danwarman <me@danwarman.co.uk>
2020-05-27 15:08:18 +0100

    FIX: Fix getKeyType to return number

--- src/js/helpers/keyData.js
+++ src/js/helpers/keyData.js
@@ -1,12 +1,12 @@
 export const getKeyType = (key) => {
   const { dataset } = key;
 
-  if (!dataset) return 'number';
   if (dataset.decimal) return 'decimal';
   if (dataset.action && dataset.action === 'clear') return 'clear';
   if (dataset.action && dataset.action === 'save') return 'save';
   if (dataset.operator && dataset.operator !== 'equals') return 'operator';
   if (dataset.operator && dataset.operator === 'equals') return 'equals';
+  return 'number';
 };
 
 export const getKeyContent = (key) => {
