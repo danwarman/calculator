ee11f26d5711a41190571de87ce3b3f1ac55d069
danwarman <me@danwarman.co.uk>
2020-05-27 14:46:44 +0100

    FIX: Forgot to save DOM selectors

---
+++ src/js/helpers/domSelectors.js
@@ -0,0 +1,7 @@
+export const calc = document.querySelector('.calc');
+export const calcDisplay = calc.querySelector('.calc-display');
+
+export const displayEquation = calcDisplay.querySelector('.calc-display__equation');
+export const displayResult = calcDisplay.querySelector('.calc-display__result');
+
+export const keys = calc.querySelector('.calc-keypad');
